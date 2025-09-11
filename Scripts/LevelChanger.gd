85a6b59238d1edec051c7945475e765399b54ec0
diluvio-eljuego <chavx.bien@gmail.com>
2025-09-10 18:28:12 -0600

    coomit

--- Scripts/LevelChanger.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-signal changeLevel(nextLevel);
-
-func requestLevelChange(levelName : String) -> void:
-	emit_signal("changeLevel", levelName);
--- escenas/outro.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-
-func _on_video_stream_player_finished() -> void:
-	OS.shell_open("https://bfan.link/chavos-bien-2")
